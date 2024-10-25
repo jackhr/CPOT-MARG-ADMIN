8fd64102b5062c2518084aed93f51ead73000f7f
jackhr <jc2o@mac.com>
2024-10-25 11:55:01 -0400

    1) Add icons 2) Adjust colors

--- app/views/admin/dashboard.php
+++ app/views/admin/dashboard.php
@@ -3,16 +3,109 @@
 
 <div id="dashboard-container">
     <div id="dashboard-main">
-        <a href="/dashboard">Sconces</a>
-        <a href="/dashboard">Covers</a>
-        <a href="/dashboard">Cut Outs</a>
-        <a href="/dashboard">Finishes</a>
-        <a href="/dashboard">Lights</a>
-        <a href="/dashboard">Orders</a>
-        <a href="/dashboard">Promotions</a>
-        <a href="/dashboard">Roles</a>
-        <a href="/users">Users</a>
-        <a href="/dashboard">Unique Ceramics</a>
+        <a href="/dashboard">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M11 4h6l3 7H8l3-7Z"></path>
+                <path d="M14 11v5a2 2 0 0 1-2 2H8"></path>
+                <path d="M4 15h2a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2H4v-6Z"></path>
+            </svg>
+            <span>Sconces</span>
+        </a>
+        <a href="/dashboard">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M12 10V2" />
+                <path d="m4.93 10.93 1.41 1.41" />
+                <path d="M2 18h2" />
+                <path d="M20 18h2" />
+                <path d="m19.07 10.93-1.41 1.41" />
+                <path d="M22 22H2" />
+                <path d="m16 6-4 4-4-4" />
+                <path d="M16 18a4 4 0 0 0-8 0" />
+            </svg>
+            <span>Covers</span>
+        </a>
+        <a href="/dashboard">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M5.42 9.42 8 12" />
+                <circle cx="4" cy="8" r="2" />
+                <path d="m14 6-8.58 8.58" />
+                <circle cx="4" cy="16" r="2" />
+                <path d="M10.8 14.8 14 18" />
+                <path d="M16 12h-2" />
+                <path d="M22 12h-2" />
+            </svg>
+            <span>Cut Outs</span>
+        </a>
+        <a href="/dashboard">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z" />
+                <path d="M20 3v4" />
+                <path d="M22 5h-4" />
+                <path d="M4 17v2" />
+                <path d="M5 18H3" />
+            </svg>
+            <span>Finishes</span>
+        </a>
+        <a href="/dashboard">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5" />
+                <path d="M9 18h6" />
+                <path d="M10 22h4" />
+            </svg>
+            <span>Lights</span>
+        </a>
+        <a href="/dashboard">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M15 12h-5" />
+                <path d="M15 8h-5" />
+                <path d="M19 17V5a2 2 0 0 0-2-2H4" />
+                <path d="M8 21h12a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v1a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v2a1 1 0 0 0 1 1h3" />
+            </svg>
+            <span>Orders</span>
+        </a>
+        <a href="/dashboard">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M2 9a3 3 0 0 1 0 6v2a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-2a3 3 0 0 1 0-6V7a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z" />
+                <path d="m9 12 2 2 4-4" />
+            </svg>
+            <span>Promotions</span>
+        </a>
+        <a href="/dashboard">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <circle cx="18" cy="15" r="3" />
+                <circle cx="9" cy="7" r="4" />
+                <path d="M10 15H6a4 4 0 0 0-4 4v2" />
+                <path d="m21.7 16.4-.9-.3" />
+                <path d="m15.2 13.9-.9-.3" />
+                <path d="m16.6 18.7.3-.9" />
+                <path d="m19.1 12.2.3-.9" />
+                <path d="m19.6 18.7-.4-1" />
+                <path d="m16.8 12.3-.4-1" />
+                <path d="m14.3 16.6 1-.4" />
+                <path d="m20.7 13.8 1-.4" />
+            </svg>
+            <span>Roles</span>
+        </a>
+        <a href="/users">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                <circle cx="9" cy="7" r="4" />
+                <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+                <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+            </svg>
+            <span>Users</span>
+        </a>
+        <a href="/dashboard">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M10 2v5.632c0 .424-.272.795-.653.982A6 6 0 0 0 6 14c.006 4 3 7 5 8" />
+                <path d="M10 5H8a2 2 0 0 0 0 4h.68" />
+                <path d="M14 2v5.632c0 .424.272.795.652.982A6 6 0 0 1 18 14c0 4-3 7-5 8" />
+                <path d="M14 5h2a2 2 0 0 1 0 4h-.68" />
+                <path d="M18 22H6" />
+                <path d="M9 2h6" />
+            </svg>
+            <span>Unique Ceramics</span>
+        </a>
         <a class="ghost">Ghost</a>
         <a class="ghost">Ghost</a>
     </div>
