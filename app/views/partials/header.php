ebfb42ce4c63a3e54317cd47cbb37df770508089
jackhr <jc2o@mac.com>
2024-10-28 06:05:46 -0400

    Change html title value

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Yuuuuuup</title>
+    <title><?php echo getenv("COMPANY_NAME"); ?></title>
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/DataTables/datatables.min.css">
 
