33e072fb2afc10b7791c4da98e5f0649ce3bcdbc
jackhr <jc2o@mac.com>
2024-10-31 14:00:45 -0400

    Make sure js files are called synchronously

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -14,10 +14,10 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <script src="/assets/js/jquery/jquery-3.7.1.min.js"></script>
-    <script src="/assets/js/DataTables/datatables.min.js" defer></script>
-    <script src="/assets/js/hongymagic-jQuery.serializeObject/dist/jquery.serializeObject.min.js" defer></script>
-    <script src="/assets/js/sweetalert2/js/sweetalert2.all.min.js" defer></script>
-    <script src="/assets/js/main.js" defer></script>
+    <script src="/assets/js/DataTables/datatables.min.js"></script>
+    <script src="/assets/js/hongymagic-jQuery.serializeObject/dist/jquery.serializeObject.min.js"></script>
+    <script src="/assets/js/sweetalert2/js/sweetalert2.all.min.js"></script>
+    <script src="/assets/js/main.js"></script>
 </head>
 
 <body>
