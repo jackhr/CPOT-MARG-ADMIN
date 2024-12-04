92b2af69b72f9b304244be808693085b9ff2fb04
jackhr <jc2o@mac.com>
2024-12-04 13:59:40 -0500

    Add flickity plugin

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -5,15 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo getenv("COMPANY_NAME"); ?></title>
-    <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/DataTables/datatables.min.css">
     <link rel="stylesheet" href="/assets/css/DataTables/override.css">
+    <link rel="stylesheet" href="/assets/js/Flickity/css/flickity.min.css" media="screen">
+    <link rel="stylesheet" href="/assets/js/Flickity/css/flickity-fade.css" media="screen">
+    <link rel="stylesheet" href="/assets/css/style.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <script src="/assets/js/jquery/jquery-3.7.1.min.js"></script>
+    <script src="/assets/js/Flickity/js/flickity.pkgd.min.js"></script>
+    <script src="/assets/js/Flickity/js/flickity-fade.js"></script>
+    <script src="/assets/js/Flickity/js/fullscreen.js"></script>
+    <script src="/assets/js/Flickity/js/flickity-imagesloaded.js"></script>
     <script src="/assets/js/DataTables/datatables.min.js"></script>
     <script src="/assets/js/hongymagic-jQuery.serializeObject/dist/jquery.serializeObject.min.js"></script>
     <script src="/assets/js/sweetalert2/js/sweetalert2.all.min.js"></script>
