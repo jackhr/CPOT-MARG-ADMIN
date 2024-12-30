783c1424b219b14e9910993b9ce935bdc962905d
jackhr <jc2o@mac.com>
2024-12-30 07:56:14 -0400

    Add favicon

--- app/views/partials/header.php
+++ app/views/partials/header.php
@@ -11,6 +11,13 @@
     <link rel="stylesheet" href="/assets/js/Flickity/css/flickity-fade.css" media="screen">
     <link rel="stylesheet" href="/assets/css/style.css">
 
+    <!-- favicon begin -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/assets/images/favicon/site.webmanifest">
+    <!-- favicon end -->
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
